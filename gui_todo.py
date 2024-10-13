0a5f4789b3806a9627bc9db49a66557d7c1e1456
leekempson <leekempson@hotmail.com>
2024-10-13 23:35:20 +0200

    gui blueprint

---
+++ gui_todo.py
@@ -0,0 +1,17 @@
+import functions
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="enter todo")
+
+"""Window contailns layout of the gui. Below there are 2 layout options"""
+#window = sg.Window('My To-Do App', layout=[[label, input_box]])
+
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+
+
+
+window.read()
+window.close()
